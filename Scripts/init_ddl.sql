b2fda24216477cbaba4c67dc8cbb3784c0f95c95
satwika78 <88225043+satwika78@users.noreply.github.com>
2025-10-07 17:02:26 -0500

    Create init_ddl.sql

---
+++ Scripts/init_ddl.sql
@@ -0,0 +1,30 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+--Creating a database
+CREATE DATABASE DataWarehouse;
+GO
+
+-- Set the current context to the new database
+USE DataWarehouse;
+GO
+
+--Create the schemas for the Medallion Architecture
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
